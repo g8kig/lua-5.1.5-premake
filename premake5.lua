43e3c20c544c7e2e36bc37d75c492145d26b0b2f
Paul Winwood <g8kig@users.noreply.github.com>
2020-05-30 18:28:38 +0100

    Update premake5.lua reformatting only

--- premake5.lua
+++ premake5.lua
@@ -6,29 +6,29 @@ project "Lua51"
    kind "SharedLib"
    language "C"
    targetdir "bin/%{cfg.buildcfg}"
-   linkoptions "/DEF:Lua51.def"
 
    files { "src/**.h", 
 	   "src/lapi.c",    "src/lcode.c",   "src/ldebug.c",   "src/ldo.c", 
 	   "src/ldump.c",   "src/lfunc.c",   "src/lgc.c",      "src/llex.c", 
 	   "src/lmem.c",    "src/lobject.c", "src/lopcodes.c", "src/lparser.c", 
-           "src/lstate.c",  "src/lstring.c", "src/ltable.c",   "src/ltm.c", 
-           "src/lundump.c", "src/lvm.c",     "src/lzio.c",     "src/print.c" }
+      "src/lstate.c",  "src/lstring.c", "src/ltable.c",   "src/ltm.c", 
+      "src/lundump.c", "src/lvm.c",     "src/lzio.c",     "src/print.c" }
 
    filter { "action:vs*", "configurations:Release" }
-	buildoptions "/GL"
- 	linkoptions "/LTCG" 
+	   buildoptions "/GL"
+ 	   linkoptions "/LTCG" 
 
    filter "action:vs*"
-  	defines { "_CRT_SECURE_NO_WARNINGS", "LUA_BUILD_AS_DLL" }
+  	   defines { "_CRT_SECURE_NO_WARNINGS", "LUA_BUILD_AS_DLL" }
+      linkoptions "/DEF:Lua51.def"
 
    filter "configurations:Debug"
-      	defines { "DEBUG" }
-      	symbols "On"
+      defines { "DEBUG" }
+      symbols "On"
 
    filter "configurations:Release"
    	defines { "NDEBUG" }
-      	optimize "On"
+      optimize "On"
 
 project "Lua51Lib"
    kind "SharedLib"
@@ -36,27 +36,27 @@ project "Lua51Lib"
    targetdir "bin/%{cfg.buildcfg}"
    links { "Lua51" }
    libdirs { "bin/%{cfg.buildcfg}" }
-   linkoptions "/DEF:Lua51Lib.def"
 
    files { "src/**.h", 
 	   "src/lauxlib.c",  "src/lbaselib.c", "src/ldblib.c",  "src/liolib.c", 
-           "src/lmathlib.c", "src/lstrlib.c",  "src/loadlib.c", "src/loslib.c", 
-           "src/ltablib.c",  "src/linit.c"}
+      "src/lmathlib.c", "src/lstrlib.c",  "src/loadlib.c", "src/loslib.c", 
+      "src/ltablib.c",  "src/linit.c"}
 
    filter { "action:vs*", "configurations:Release" }
-	buildoptions "/GL"
- 	linkoptions "/LTCG" 
+	   buildoptions "/GL"
+ 	   linkoptions "/LTCG" 
 
    filter "action:vs*"
   	defines { "_CRT_SECURE_NO_WARNINGS", "LUA_BUILD_AS_DLL" }
+   	linkoptions "/DEF:Lua51Lib.def"
 
    filter "configurations:Debug"
-      	defines { "DEBUG" }
-      	symbols "On"
+      defines { "DEBUG" }
+      symbols "On"
 
    filter "configurations:Release"
    	defines { "NDEBUG" }
-      	optimize "On"
+      optimize "On"
 
 project "Lua"
    kind "ConsoleApp"
@@ -64,23 +64,22 @@ project "Lua"
    targetdir "bin/%{cfg.buildcfg}"
    links { "Lua51", "Lua51lib" }
    libdirs { "bin/%{cfg.buildcfg}" }
-
    files { "src/**.h", "src/lua.c" }
 
    filter { "action:vs*", "configurations:Release" }
-	buildoptions "/GL"
- 	linkoptions "/LTCG" 
+	   buildoptions "/GL"
+ 	   linkoptions "/LTCG" 
 
    filter "action:vs*"
-  	defines { "_CRT_SECURE_NO_WARNINGS", "LUA_BUILD_AS_DLL" }
+  	   defines { "_CRT_SECURE_NO_WARNINGS", "LUA_BUILD_AS_DLL" }
 
    filter "configurations:Debug"
-      	defines { "DEBUG" }
-      	symbols "On"
+      defines { "DEBUG" }
+      symbols "On"
 
    filter "configurations:Release"
    	defines { "NDEBUG" }
-      	optimize "On"
+      optimize "On"
 
 project "Luac"
    kind "ConsoleApp"
@@ -91,11 +90,11 @@ project "Luac"
 
    files { "src/**.h", 
 	   "src/luac.c",    "src/ldo.c",     "src/lgc.c",     "src/lfunc.c", 
-           "src/ltable.c",  "src/lvm.c",     "src/lstate.c",  "src/ldebug.c", 
-           "src/lobject.c", "src/lapi.c",    "src/lmem.c",    "src/lopcodes.c", 
-           "src/lzio.c",    "src/lstring.c", "src/ldump.c",   "src/llex.c", 
-           "src/ltm.c",     "src/print.c",   "src/lundump.c", "src/lparser.c", 
-           "src/lcode.c" }
+      "src/ltable.c",  "src/lvm.c",     "src/lstate.c",  "src/ldebug.c", 
+      "src/lobject.c", "src/lapi.c",    "src/lmem.c",    "src/lopcodes.c", 
+      "src/lzio.c",    "src/lstring.c", "src/ldump.c",   "src/llex.c", 
+      "src/ltm.c",     "src/print.c",   "src/lundump.c", "src/lparser.c", 
+      "src/lcode.c" }
 
    filter { "action:vs*", "configurations:Release" }
 	buildoptions "/GL"
@@ -105,9 +104,9 @@ project "Luac"
   	defines { "_CRT_SECURE_NO_WARNINGS", "LUA_BUILD_AS_DLL" }
 
    filter "configurations:Debug"
-      	defines { "DEBUG" }
-      	symbols "On"
+      defines { "DEBUG" }
+      symbols "On"
 
    filter "configurations:Release"
    	defines { "NDEBUG" }
-      	optimize "On"
+      optimize "On"
